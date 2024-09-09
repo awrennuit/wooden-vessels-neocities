27e39f42cbe47e8913212d8163b36f510f528020
AwrenNuit <a.rennaker@yahoo.com>
2024-09-09 11:59:52 -0500

    update news; add border between sections; set up most of the code for a classic cursor trail

---
+++ public/scripts.js
@@ -0,0 +1,16 @@
+// TOOD: add throttle/debounce
+// function trail(e) {
+//   const $div = document.createElement('div');
+
+//   $div.classList.add('trail');
+
+//   Object.assign($div.style, {
+//     top: e.pageY + 'px',
+//     left: e.pageX + 'px',
+//   });
+
+//   document.body.appendChild($div);
+//   setTimeout(() => document.body.removeChild($div), 500);
+// }
+
+// addEventListener('mousemove', trail);
